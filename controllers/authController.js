d81a05368944ebbb29ac2fa539ca0ae198b26568
Kenneth Jimmy <kenjimmy17@gmail.com>
2021-03-18 10:27:43 +0100

    🎗 Rename folders

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,8 +2,8 @@ const crypto = require('crypto');
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
-const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
