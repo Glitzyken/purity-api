5c368cda0b0bba9bd87434798a8c2359255acd3f
Kenneth Jimmy <kenjimmy17@gmail.com>
2021-03-18 09:06:17 +0100

    ⬇ Install fundamental dependecies

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
 const express = require("express");
+
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 const userRouter = require("./routes/userRoute");
 
+// Start express app
 const app = express();
 
-// Body parser
-app.use(express.json());
+app.enable("trust proxy");
+
+// GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+app.options("*", cors());
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 app.use("/user", userRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
